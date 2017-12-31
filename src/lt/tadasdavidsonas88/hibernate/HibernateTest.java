c75ea1e51410fdc1e980a61d193e6578ddcd0ec3
tdavidsonas88 <32385991+tdavidsonas88@users.noreply.github.com>
2017-12-31 17:25:18 +0200

    select and pagination in HQL

--- src/lt/tadasdavidsonas88/hibernate/HibernateTest.java
+++ src/lt/tadasdavidsonas88/hibernate/HibernateTest.java
@@ -7,6 +7,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
+import lt.tadasdavidsonas88.dto.UserDetails;
+
 public class HibernateTest {
 
 	public static void main(String[] args) {
@@ -15,12 +17,19 @@ public class HibernateTest {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 
-		Query query = session.createQuery("from USER_DETAILS where userId > 5");
-		List users = query.list();
+		Query query = session.createQuery("select userName from USER_DETAILS where userId > 5");
+		// --- pagination -----
+		query.setFirstResult(0);
+		query.setMaxResults(3);
+		// --------------------
+		List<String> userNames = (List<String>) query.list();
 		
 		session.getTransaction().commit();
 		session.close();
-		System.out.println("Size of list result = " + users.size());
+		System.out.println("Size of list result = " + userNames.size());
+		
+		for (String u : userNames)
+			System.out.println(u);
 		
 	}
 
