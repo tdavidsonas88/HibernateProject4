9d461cf58af9777176226ed2bf804d5ea1cca407
tdavidsonas88 <32385991+tdavidsonas88@users.noreply.github.com>
2018-01-02 12:47:31 +0200

    parameter binding to avoid SQL injection

--- src/lt/tadasdavidsonas88/hibernate/HibernateTest.java
+++ src/lt/tadasdavidsonas88/hibernate/HibernateTest.java
@@ -7,20 +7,23 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-import lt.tadasdavidsonas88.dto.UserDetails;
-
 public class HibernateTest {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 			
 		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
+		String minUserId = "5"; 	
 
-		Query query = session.createQuery("select userName from USER_DETAILS where userId > 5");
+		Query query = session.createQuery("select userName from USER_DETAILS where userId > :userId");
+//		Query query = session.createQuery("select userName from USER_DETAILS where userId > ?);
+		query.setInteger("userId", Integer.parseInt(minUserId));
+//		query.setInteger(0, Integer.parseInt(minUserId));
 		// --- pagination -----
-		query.setFirstResult(0);
-		query.setMaxResults(3);
+//		query.setFirstResult(0);
+//		query.setMaxResults(3);
 		// --------------------
 		List<String> userNames = (List<String>) query.list();
 		
